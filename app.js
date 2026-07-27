62f3ad3e2ca6affc3ecd5dd5b449590ba6b069c3
LewisEverythingNeer <143146597+LewisRos1@users.noreply.github.com>
2026-07-27 11:29:18 +0800

    v1139: Activity filter box blinks 3 times

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function versionLineText() {
   return `Made by Lewis · Version ${APP_VERSION}`;
 }
 
-const APP_VERSION = "1138";
+const APP_VERSION = "1139";
 
 // ─── STATE ───────────────────────────────────────────────────
 const state = {
