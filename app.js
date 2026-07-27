6dc04009455ea70342e51c71efe97d0c5cae87ec
LewisEverythingNeer <143146597+LewisRos1@users.noreply.github.com>
2026-07-27 11:19:40 +0800

    v1137: Activity filter box blinks 4 times then stops

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function versionLineText() {
   return `Made by Lewis · Version ${APP_VERSION}`;
 }
 
-const APP_VERSION = "1136";
+const APP_VERSION = "1137";
 
 // ─── STATE ───────────────────────────────────────────────────
 const state = {
