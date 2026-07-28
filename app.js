030ffed3126853ba9a0851c54b8631081a71d7eb
LewisEverythingNeer <143146597+LewisRos1@users.noreply.github.com>
2026-07-28 16:15:40 +0800

    v1188: Reload button adds clarification text

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ function versionLineText() {
   return `Made by Lewis · Version ${APP_VERSION}`;
 }
 
-const APP_VERSION = "1187";
+const APP_VERSION = "1188";
 
 // ─── STATE ───────────────────────────────────────────────────
 const state = {
